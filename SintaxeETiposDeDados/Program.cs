9925c60d89ed2b8af7d26a19519578b78d682bf2
RafaelaSoliva <rafaelasoliva@outlook.com>
2024-12-29 18:34:32 -0300

    Feat: Desafio Sintaxe e Tipos de Dados

---
+++ SintaxeETiposDeDados/Program.cs
@@ -0,0 +1,24 @@
+﻿// O objetivo é desenvolver uma solução simulando o registro de usuário em uma aplicação. 
+// O usuário deve informar o email e o nome de usuário e a aplicação deve retornar uma mensagem de confirmação.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+      // Declaração de variáveis
+      string email;
+      string nomeUsuario;
+      string senha;
+
+      // Obtém o email e nome do usuário a partir da entrada do console
+      email = Console.ReadLine();
+      nomeUsuario = Console.ReadLine();
+
+      // Retorno para o usuário
+      Console.WriteLine($"{nomeUsuario}, verifique o email: {email} para ativar.");
+      
+    } 
+}
