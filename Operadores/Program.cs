62c480e266c18fa68927f340fe78255d47f52a71
RafaelaSoliva <rafaelasoliva@outlook.com>
2024-12-29 18:44:17 -0300

    Feat: Operadores

---
+++ Operadores/Program.cs
@@ -0,0 +1,30 @@
+﻿// O objetivo é a simulação de um Sistema de Gerenciamento de Tarefas.
+// O programa deve receber o título e a descrição de uma tarefa e, caso a descrição não ultrapasse 50 caracteres,
+// deve retornar a descrição e a data de vencimento da tarefa.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+      // Declaração de variáveis
+      string titulo;
+      string descricao;
+      string dataVencimento;
+        
+      // Obtém o titulo e a descricao a partir da entrada do console  
+      titulo = Console.ReadLine();
+      descricao = Console.ReadLine();
+
+      // Estrutura condicional 'if/else' para verificar se a descrição da tarefa excede 50 caracteres
+      if (descricao.Length <= 50) {
+        dataVencimento = Console.ReadLine();
+        Console.WriteLine($"{descricao} ate {dataVencimento}");
+        } else {
+        Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+        }
+        
+    }
+}
